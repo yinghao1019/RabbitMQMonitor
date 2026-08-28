691211522962252118c19cd60974a3098156d566
Howard.YH.Hung <howard.yh.hung@fii-foxconn.com>
2026-08-28 17:19:29 +0800

    feat: rabbit mq configs

---
+++ Configs/RabbitMQConfig.cs
@@ -0,0 +1,11 @@
+
+
+namespace RabbitMQMonitor.Configs
+{
+    public class RabbitMQConfig
+    {
+        public string Url { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
