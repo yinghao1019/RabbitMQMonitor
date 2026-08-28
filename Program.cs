bbf549dc0f24f05cbe4fb87d719c8fe7b28f8ebd
Howard.YH.Hung <howard.yh.hung@fii-foxconn.com>
2026-08-28 17:16:11 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace RabbitMQMonitor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        using var host = builder.Build();
+    }
+}
